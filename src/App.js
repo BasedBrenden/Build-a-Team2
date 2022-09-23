bf8c93176c0c53e4c08c6e7d8cebdf24f1d3dfda
Brenden Thomas <brenden.thomas17@gmail.com>
2022-09-23 16:19:02 -0500

    Added firebase security settings

--- src/App.js
+++ src/App.js
@@ -5,6 +5,48 @@ import TeamView from './components/TeamView';
 import Navbar from './components/Nav/Navbar';
 
 
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDj04TlOHQPhjj87M_o3Egih04g5hAROVc",
+
+  authDomain: "buildateambt.firebaseapp.com",
+
+  databaseURL: "https://buildateambt-default-rtdb.firebaseio.com",
+
+  projectId: "buildateambt",
+
+  storageBucket: "buildateambt.appspot.com",
+
+  messagingSenderId: "231750263107",
+
+  appId: "1:231750263107:web:e20e2f15ba084655c04ff6",
+
+  measurementId: "G-SSTG0YXNM4"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
+
 
 function App() {
 
--- src/index.js
+++ src/index.js
@@ -8,52 +8,6 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import LogInForm from './components/Nav/LogIn/LogInPage';
 
-// Import the functions you need from the SDKs you need
-
-import { initializeApp } from "firebase/app";
-
-import { getAnalytics } from "firebase/analytics";
-
-// TODO: Add SDKs for Firebase products that you want to use
-
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-
-// Your web app's Firebase configuration
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-const firebaseConfig = {
-
-  apiKey: "AIzaSyAwIVM34CSefM9DoaHEEYc_koEndy2VNh0",
-
-  authDomain: "battest-be17d.firebaseapp.com",
-
-  projectId: "battest-be17d",
-
-  storageBucket: "battest-be17d.appspot.com",
-
-  messagingSenderId: "436395671464",
-
-  appId: "1:436395671464:web:15ae73b0ffbc8670ee679d",
-
-  measurementId: "G-PD5SCY0JY1"
-
-};
-
-
-// Initialize Firebase
-
-const app = initializeApp(firebaseConfig);
-
-const analytics = getAnalytics(app);
-
-
-
-
-
-
-
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
