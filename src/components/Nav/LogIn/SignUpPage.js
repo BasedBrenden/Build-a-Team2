c46eacc77439e339da323f271ece6a0bf54fea32
Brenden Thomas <brenden.thomas17@gmail.com>
2022-09-23 17:54:53 -0500

    removed comment

--- src/components/Nav/LogIn/SignUpPage.js
+++ src/components/Nav/LogIn/SignUpPage.js
@@ -11,17 +11,7 @@ const SignUpForm = () =>{
             username: document.getElementById("username").value,
             password: document.getElementById("password").value
         }
-        /*https://batbackend.herokuapp.com
-        http://localhost:3000/*/
-        /*fetch('http://localhost:3000/sign-up',{
-        method: 'POST',
-        headers: {'Content-Type': 'application/json' },
-        body: JSON.stringify({username1: document.getElementById("username").value,
-        password1: document.getElementById("password").value}),
-        })
-        .then((response) => {console.log(response)})
-        .catch((error) => alert(error))
-        console.log(userInfo);*/
+        
 
         createUserWithEmailAndPassword(auth, userInfo.username, userInfo.password)
         .then((userCredential) => {
