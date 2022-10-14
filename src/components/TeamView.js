b63b1f66d329df8daf3ef13e8ab1213f4060a7ca
Brenden Thomas <brenden.thomas17@gmail.com>
2022-10-13 22:01:47 -0500

    Feat: Added type styling

--- src/components/TeamView.js
+++ src/components/TeamView.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './TeamView.css'
 
 
@@ -8,7 +8,64 @@ const TeamView = ({fullTeam, updateTeamFunc, userId}) => {
 
     const [focusedPokemon, setFocusedPokemon] = useState('');
     const [focusedAbility, setFocusedAbility] = useState('');
-    const updateError = document.querySelector('.errorMessage')
+    const updateError = document.querySelector('.errorMessage');
+
+    const changeTypeColor = (item) =>{
+        
+
+            if(item.innerHTML === "FIRE"){
+                item.style.backgroundColor = "Orange";
+            }else if(item.innerHTML === "FLYING"){
+                item.style.backgroundColor = "#55bec2";
+            }else if(item.innerHTML === "STEEL"){
+                item.style.backgroundColor = "#88888a";
+            }else if(item.innerHTML === "WATER"){
+                item.style.backgroundColor = "#0909e3";
+            }else if(item.innerHTML === "GRASS"){
+                item.style.backgroundColor = "#049413";
+            }else if(item.innerHTML === "DARK"){
+                item.style.backgroundColor = "#010801";
+            }else if(item.innerHTML === "ELECTRIC"){
+                item.style.backgroundColor = "#bdb000";
+            }else if(item.innerHTML === "BUG"){
+                item.style.backgroundColor = "#a9b820";
+            }else if(item.innerHTML === "PSYCHIC"){
+                item.style.backgroundColor = "#f85888";
+            }else if(item.innerHTML === "ROCK"){
+                item.style.backgroundColor = "#b8a038";
+            }else if(item.innerHTML === "GROUND"){
+                item.style.backgroundColor = "#927d44";
+            }else if(item.innerHTML === "FAIRY"){
+                item.style.backgroundColor = "#9b6470";
+            }else if(item.innerHTML === "FIGHTING"){
+                item.style.backgroundColor = "#c03028";
+            }else if(item.innerHTML === "GHOST"){
+                item.style.backgroundColor = "#705898";
+            }else if(item.innerHTML === "ICE"){
+                item.style.backgroundColor = "#638d8d";
+            }else if(item.innerHTML === "NORMAL"){
+                item.style.backgroundColor = "#6d6d4e";
+            }else if(item.innerHTML === "POISON"){
+                item.style.backgroundColor = "#682a68";
+            }else if(item.innerHTML === "DRAGON"){
+                item.style.backgroundColor = "#7238f8";
+            }else{
+                return;
+            }
+
+    }
+
+    useEffect(() => {
+        if(document.querySelector(".testClass")){
+            const typeSelected = document.querySelector(".testClass");
+            changeTypeColor(typeSelected)
+        }
+        if(document.querySelector(".testClass2")){
+            const typeSelected2 = document.querySelector(".testClass2");
+            changeTypeColor(typeSelected2)
+        }
+    }, [focusedPokemon])
+    
 
     const addPokemonToTeam = () =>{
 
@@ -137,7 +194,7 @@ const TeamView = ({fullTeam, updateTeamFunc, userId}) => {
                     <div id="infoStats">
                         <div className="statTitle">
                             <p>Dex No.:</p>
-                            <p>Type:</p>
+                            <p className = "statTitleType">Type:</p>
                             <p>OT:</p>
                             <p>OT ID:</p>
                             <p>Advantage: </p>
@@ -146,7 +203,12 @@ const TeamView = ({fullTeam, updateTeamFunc, userId}) => {
                         <div className="statContent">
 
                             <p>{focusedPokemon.pokeID}</p>
-                            <p>{focusedPokemon.pokeType} {focusedPokemon.pokeType2}</p>
+                            <p>
+                            <div className="typeContainer">
+                            <span className ="testClass">{focusedPokemon.pokeType}</span>
+                            {(focusedPokemon.pokeType2 === '') ?<span></span>: <span className ="testClass2">{focusedPokemon.pokeType2}</span>}
+                            </div>
+                            </p>
                             <p>Brenden</p>
                             <p> 1776 </p>
                             <p>n/a</p>
