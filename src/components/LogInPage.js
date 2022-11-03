e39d09c863bc5befa605b23cce32baf9e44c5096
Brenden Thomas <brenden.thomas17@gmail.com>
2022-11-03 15:14:06 -0500

    Refactor: disable es-lint anchor-is-valid

--- src/components/LogInPage.js
+++ src/components/LogInPage.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react"
 import {  auth, signInWithEmailAndPassword } from "../firebase";
 import { useNavigate } from "react-router-dom";
