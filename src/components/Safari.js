245ef36ed0924ad6b78e0d1760b00f75003143d1
Brenden Thomas <brenden.thomas17@gmail.com>
2023-01-29 19:25:49 -0600

    Added Safari button styling

--- src/components/Safari.js
+++ src/components/Safari.js
@@ -15,6 +15,8 @@ const Safari = () =>{
         bait: 10
     })
     const [caughtPokemon, setCaughtPokemon] = useState([])
+    const [buttonState, setButtonState] = useState(false)
+    
     useEffect(() => {
       
     startGame()
@@ -39,6 +41,9 @@ const Safari = () =>{
         const pokeNameH1 = document.querySelector('#pokeName')
         const pokeImg = document.querySelector('#pokeSprite')
         const promptP = document.querySelector('#prompt')
+        const itemHold = document.querySelector('#item')
+
+        itemHold.classList = " "
 
         let availPokemon = [0]
         for(let i=0; i < 50; i++){
@@ -46,8 +51,10 @@ const Safari = () =>{
         }
         
         const id = availPokemon[Math.floor(Math.random()*49)]
+        pokeImg.classList = " "
         pokeImg.classList.add("dot-flashing")
         pokeImg.src = " "
+        
         pokeNameH1.innerHTML = "Finding new Safari Encounter..."
 
         const selectedPokemon = await (await fetch('https://pokeapi.co/api/v2/pokemon/'+ id)).json()
@@ -73,107 +80,151 @@ const Safari = () =>{
     }
 
     const throwPokeball = () =>{
-        const promptP = document.querySelector('#prompt')
-        promptP.innerHTML = "Go! Pokeball!"
-        //execute pokeball throw animation
+        if(!buttonState){
+            const promptP = document.querySelector('#prompt')
+            promptP.innerHTML = "Go! Pokeball!"
+            //execute pokeball throw animation
 
-        let newPokeballCount = itemInventory.pokeBalls - 1
-        
-        if (newPokeballCount < 0){
-            newPokeballCount = 0
-            updatePrompt("You're all out of pokeballs!", 2000)
-        }else{
-            if ((Math.random()) < pokeStatus.catchRate ){
-                updatePrompt("Got em!",6000)
-                setTimeout(startGame, 9000)
-                setCaughtPokemon([...caughtPokemon,document.querySelector('#pokeName').innerHTML])
+            const pokeSprite = document.querySelector('#pokeSprite')
+            const pokeball = document.querySelector("#item")
+            
+            pokeball.classList = "pokeball"
+            
+            setTimeout(()=>{
+                pokeSprite.classList.add("shrinkSprite")
+            },500)
+
+            
+
+            let newPokeballCount = itemInventory.pokeBalls - 1
+            if (newPokeballCount < 0){
+                blockButtons(9001)
+                newPokeballCount = 0
+                updatePrompt("You're all out of pokeballs!", 2000)
+            
             }else{
-                updatePrompt("Darn, so close!",6000)
-                if((Math.random()) < pokeStatus.fleeChance){
-                    updatePrompt("The pokemon ran away...",9000)
-                    setTimeout(startGame, 12000)
+                blockButtons(9001)
+                if ((Math.random()) < pokeStatus.catchRate ){
+                    updatePrompt("Got em!",6000)
+                    pokeSprite.classList = ""
+                    
+                    setTimeout(startGame, 9000)
+                    setCaughtPokemon([...caughtPokemon,document.querySelector('#pokeName').innerHTML])
+                }else{
+                    setTimeout(()=>{
+                        pokeSprite.classList.remove("shrinkSprite")
+                        pokeSprite.classList.add("growSprite")
+                        pokeball.classList.remove("pokeball")
+                    },7000)
+                    updatePrompt("Darn, so close!",6000)
+                    
+                    
+                    if((Math.random()) < pokeStatus.fleeChance){
+                        updatePrompt("The pokemon ran away...",9000)
+                        setTimeout(startGame, 12000)
+                    }
                 }
             }
+            setItemInventory(inventory=>({...itemInventory, pokeBalls: newPokeballCount}))
+            console.log(caughtPokemon)
+            for (let i = 0; i < pokeSprite.classList.length; i++) {
+                pokeSprite.classList.remove(pokeSprite.classList[i]);
+              }
+            
+            updatePrompt("What would you like to do?",9000)
+            
+            //calculate chance of being caught
+            //if caught, say so
+            //if not, pokeballs down by one, flee chance up one
         }
-        setItemInventory(inventory=>({...itemInventory, pokeBalls: newPokeballCount}))
-        
-       
-        console.log(pokeStatus.catchRate, pokeStatus.fleeChance, pokeStatus.angerPercentage)
-        console.log(caughtPokemon)
-
-
-        
-        
-        
-        
-        updatePrompt("What would you like to do?",9000)
-        //calculate chance of being caught
-        //if caught, say so
-        //if not, pokeballs down by one, flee chance up one
         
     }
 
     //should bait make pokemon easier to catch?
 
     const throwBait = () =>{
-
-        //check if you have bait!
-        const baitInventory = itemInventory.bait-1
-        console.log({baitInventory})
-        if(baitInventory < 0){
-            updatePrompt("You don't have any bait!",1000)
-        }else{
-
+        if(!buttonState){
+            blockButtons(6001)
+            //check if you have bait!
+            const baitInventory = itemInventory.bait-1
+            console.log({baitInventory})
+            if(baitInventory < 0){
+                updatePrompt("You don't have any bait!",1000)
+            }else{
             //execute bait throw animation
-            const promptP = document.querySelector('#prompt')
-            promptP.innerHTML = `Threw Bait!`
-            updatePrompt( "They ate the bait! Yum!", 3000)
-            
-            const newAngerPerc = pokeStatus.angerPercentage -5
-            setPokeStatus(status =>({...status,angerPercentage: newAngerPerc}))
-            setItemInventory(inventory=>({...inventory, bait: baitInventory}))
-        }
-        
+                const promptP = document.querySelector('#prompt')
+                promptP.innerHTML = `Threw Bait!`
 
-        updatePrompt("What would you like to do?", 6000)
-        console.log({pokeStatus})
+                
+                const pokeball = document.querySelector("#item")
+                
+                pokeball.classList = "bait"
+                
+
+                setTimeout(()=>{
+                    pokeball.classList.remove("bait")
+                }, 2000)
+                updatePrompt( "They ate the bait! Yum!", 3000)
+                const newAngerPerc = pokeStatus.angerPercentage -5
+                const newCatchPerc = pokeStatus.catchRate + 0.15
+                setPokeStatus(status =>({...status,angerPercentage: newAngerPerc, catchRate: newCatchPerc}))
+                setItemInventory(inventory=>({...inventory, bait: baitInventory}))
+                
+            }
+            
+            updatePrompt("What would you like to do?", 6000)
+            console.log({pokeStatus})
+        }
+    }
+
+    const blockButtons = (interval) =>{
+        setButtonState(true)
+        setTimeout(()=>{setButtonState(false)},interval)
     }
 
     const throwRock = () =>{
         //execute rock throw animation
-        
-        updatePrompt("Hurled Rock!",0)
-        //angry poke animation
-        updatePrompt("They did NOT like that...", 3000)
-        
-        
-        
-        //decrease poke chance of running away
-        //increase anger
-        const newAngerPerc = pokeStatus.angerPercentage +10
-        const newFleeChance = pokeStatus.fleeChance + .10
-        console.log({newAngerPerc, newFleeChance})
-            //if anger too high, pokemon yells, you run away
-        if ((newAngerPerc > 100) || ((Math.random()) < newFleeChance) ){
-            const amountLost = Math.floor(Math.random()*10)
-            if(Math.random()<.45){
-                
-                updatePrompt(`The pokemon yells,causing you to flee in panic. Dropped ${amountLost} Pokeballs in panicked state`, 6000)
-                const newPokeballCount = itemInventory.pokeBalls-amountLost
-                setItemInventory(item=>({...item, pokeBalls: newPokeballCount}))
-            }else{
-                updatePrompt(`The pokemon yells,causing you to flee in panic. Dropped ${amountLost} Bait in panicked state`, 6000)
-                const newBaitCount = itemInventory.bait-amountLost
-                setItemInventory(item=>({...item, bait: newBaitCount}))
-            }
+        console.log(buttonState)
+        if(!buttonState){
+            updatePrompt("Hurled Rock!",0)
+            //angry poke animation
+
             
-            setTimeout(startGame, 9000)
-        }else{
-            setPokeStatus(status =>({...status,angerPercentage: newAngerPerc, fleeChance: newFleeChance}))
-            updatePrompt("What would you like to do?", 9000)
+            const pokeball = document.querySelector("#item")
+            
+            pokeball.classList = "rock"
+            
+
+            setTimeout(()=>{
+                pokeball.classList.remove("rock")
+            }, 2000)
+            updatePrompt("They did NOT like that...", 3000)
+            blockButtons(9001)
+            //decrease poke chance of running away
+            //increase anger
+            const newAngerPerc = pokeStatus.angerPercentage +10
+            const newFleeChance = pokeStatus.fleeChance + .10
+            console.log({newAngerPerc, newFleeChance})
+            //if anger too high, pokemon yells, you run away
+            if ((newAngerPerc > 100) || ((Math.random()) < newFleeChance) ){
+                const amountLost = Math.floor(Math.random()*10)
+                if(Math.random()<.45){
+                    updatePrompt(`The pokemon yells,causing you to flee in panic. Dropped ${amountLost} Pokeballs in panicked state`, 6000)
+                    const newPokeballCount = itemInventory.pokeBalls-amountLost
+                    setItemInventory(item=>({...item, pokeBalls: newPokeballCount}))
+                }else{
+                    updatePrompt(`The pokemon yells,causing you to flee in panic. Dropped ${amountLost} Bait in panicked state`, 6000)
+                    const newBaitCount = itemInventory.bait-amountLost
+                    setItemInventory(item=>({...item, bait: newBaitCount}))
+                }
+                setTimeout(startGame, 9000)
+            }else{
+                setPokeStatus(status =>({...status,angerPercentage: newAngerPerc, fleeChance: newFleeChance}))
+                updatePrompt("What would you like to do?", 9000)
+                
+            }
+
         }
-        
-         
     }
 
 
@@ -182,26 +233,34 @@ const Safari = () =>{
     
 
     return(
-        <div className="safariContainer">
-            <div className="nav">
+        <div>
+            <div>
                <Navbar/> 
             </div>
-
-            <div className="mainContainer">
-                <div className="safariView">
-                    <h1 id="pokeName">Finding new Safari Encounter...</h1>
-                    <img id="pokeSprite" src="" alt="   "></img>
-                </div>
-                <div className="optionsView">
-                    <p id="prompt"></p>
-                    <button type = "button" id="btnBall"onClick={()=>{throwPokeball()}}>Throw Pokeball x{itemInventory.pokeBalls}</button>
-                    <button type = "button" id="btnBait"onClick={()=>{throwBait()}}>Throw Bait x{itemInventory.bait}</button>
-                    <button type = "button" id="btnRock"onClick={()=>{throwRock()}}>Throw Rock</button>
-                    <button type = "button" id="btnRun"onClick={()=>{startGame()}}>Run</button>
+            <div className="safariContainer">
+                <div className="mainContainer">
+                    <div className="safariView">
+                        <span id="item"></span>
+                        <h1 id="pokeName">Finding new Safari Encounter...</h1>
+                        <img id="pokeSprite" src="" alt="   "></img>
+                    </div>
+                    <div className="optionsView">
+                        <p id="prompt"></p>
+                        <div className="btnContainer">
+                            <button type = "button" id="btnBall" onClick={()=>{throwPokeball()}} disabled ={buttonState}>Throw Pokeball x{itemInventory.pokeBalls}</button>
+                        </div>
+                        <div className="btnContainer">
+                            <button type = "button" id="btnBait" onClick={()=>{throwBait()}}     disabled ={buttonState}>Throw Bait x{itemInventory.bait}</button>
+                        </div>
+                        <div className="btnContainer">
+                            <button type = "button" id="btnRock" onClick={()=>{throwRock()}}     disabled ={buttonState}>Throw Rock</button>
+                        </div>
+                        <div className="btnContainer">
+                            <button type = "button" id="btnRun"  onClick={()=>{startGame()}}     disabled ={buttonState}>Run</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-            
-
         </div>
     )
 
